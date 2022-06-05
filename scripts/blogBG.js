478f3abb3062a236dbfc7e21ff39332d2b2d0b12
HopHopHasan <50119098+HopHopHasan@users.noreply.github.com>
2022-06-05 22:43:04 +0100

    bug fixes and changes

--- scripts/blogBG.js
+++ scripts/blogBG.js
@@ -2,17 +2,21 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-function checkBG(){
+function checkBG(layer){
     if (localStorage.blogBG){
       document.body.style.backgroundImage = localStorage.blogBG;
     }
     else{
-      changeBackground();
+      changeBackground(layer);
     }
 }
 
-function changeBackground(){
-  var string = "url('/resources/backgrounds/blogBG"
+function changeBackground(layer){
+  var string = "url('";
+  for (let i = 0; i < layer; i++){
+    string = string.concat("../")
+  }
+  string = string.concat("resources/backgrounds/blogBG")
   var random = getRandomInt(7)+1;
   if (localStorage.BGHQ == 1){
     var result = string.concat(random, ".png')")
@@ -21,6 +25,6 @@ function changeBackground(){
     var result = string.concat(random, ".webp')")
   }
   console.log(result);
-  localStorage.blogBG = result;
+  localStorage.blogBG = result;  
   document.body.style.backgroundImage = result;
 }
