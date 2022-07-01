aa7ec37b6b66719351a4fded1ca4c0670963e87a
HopHopHasan <50119098+HopHopHasan@users.noreply.github.com>
2022-07-01 20:42:08 +0100

    remove preloading

--- scripts/blogBG.js
+++ scripts/blogBG.js
@@ -12,7 +12,6 @@ function checkBG(layer){
     console.log("getting user background");
     if (localStorage.BGHQ == 1){
       document.body.style.backgroundImage = string + ".webp";
-      preloadBackgrounds(layer);
       var result = string.concat(localStorage.blogBG, ".png')");
     }
     else{
@@ -25,7 +24,6 @@ function checkBG(layer){
     var savedPhoto = "blogBG" + random;
     if (localStorage.BGHQ == 1){
       document.body.style.backgroundImage = savedPhoto + ".webp";
-      preloadBackgrounds(layer);
       var result = string.concat(savedPhoto, ".png')");
     }
     else{
@@ -54,35 +52,4 @@ function changeBackground(layer){
   localStorage.blogBG = savedPhoto;
   console.log(savedPhoto + " BG saved to localStorage");
   document.body.style.backgroundImage = result;
-}
-
-function preloadBackgrounds(layer){
-  var string = "";
-  for (let i = 0; i < layer; i++){
-    string = string.concat("../")
-  }
-  string = string.concat("resources/backgrounds/blogBG")
-  if (localStorage.BGHQ == 1){
-    var fileType = ".png"
-  }
-  else{
-    var fileType = ".webp"
-  }
-  var images = new Array()
-  function preload() {
-    for (i = 0; i < preload.arguments.length; i++) {
-      images[i] = new Image()
-      images[i].src = preload.arguments[i]
-    }
-  }
-  preload(
-    string+"1"+fileType,
-    string+"2"+fileType,
-    string+"3"+fileType,
-    string+"4"+fileType,
-    string+"5"+fileType,
-    string+"6"+fileType,
-    string+"7"+fileType,
-  )
-console.log("preloading done!");
 }
