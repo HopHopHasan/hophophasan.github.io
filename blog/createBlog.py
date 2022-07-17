7120e4eea9e977330d2297f911cebb12f4468520
HopHopHasan <50119098+HopHopHasan@users.noreply.github.com>
2022-07-17 19:38:58 +0100

    blog update

--- blog/createBlog.py
+++ blog/createBlog.py
@@ -1,4 +1,5 @@
-import os, subprocess, time, datetime
+import os, subprocess, time, datetime, sys
+
 #store template.txt file
 with open("template.txt", 'r') as f:
     lineCount = len(f.readlines())
@@ -6,6 +7,7 @@ with open("template.txt", 'r') as f:
 with open("template.txt", 'r') as f:
     for i in range(lineCount):
         template[i] = f.readline()
+
 #store postTemplate.txt file
 with open("postTemplate.txt", 'r') as f:
     lineCount2 = len(f.readlines())
@@ -13,7 +15,9 @@ with open("postTemplate.txt", 'r') as f:
 with open("postTemplate.txt", 'r') as f:
     for i in range(lineCount2):
         postTemplate[i] = f.readline()
+
 #blog folder config
+
 #date
 date = input("Enter the date of the blog (YYYY-MM-DD) > ")
 try:
@@ -21,38 +25,44 @@ try:
 except:
     print("Date folder already exists")
 os.chdir(date)
+
 #folder name
 folderName = input("Enter the blog folder name (eg:blog-update) > ")
 os.mkdir(folderName)
 os.chdir(folderName)
+
 #blog name
 blogName = input("Enter the blog name (eg:Blog update!) > ")
+
 #description
 desc = input("Provide a description for the page > ")
+
 #write to index.html file
 try:
     f = open("index.html", "x")
     for i in range(lineCount):
-        f.write(template[i].format(title = blogName, date = date, folderTitle = folderName, description = desc))
+        f.write(template[i].format(title = blogName, date = date, description = desc))
     print("Written!")
     f.close()
-    os.chdir("../..")
-    with open('index.html', 'r') as f:
-        data = file.readlines()
-        f.write("test")
-    for i in range(lineCount2):
-        data.insert(37+i ,fileTemplate[i])
-    with open('index.html', 'w') as f:
-        f.writelines( data )
 except:
     print("This blog already exists!")
-    os.chdir("../..")
-print("Generating log")
+    sys.exit() #stop script
 
-with open(".log.txt", 'a') as f:
+#write to index.html main file
+os.chdir("../..")
+with open('index.html', 'r', encoding="utf8") as f:
+    data = f.readlines()
+for i in range(lineCount2):
+    data.insert(36+i ,postTemplate[i].format(title = blogName, date = date, folderTitle = folderName, description = desc))
+with open('index.html', 'w', encoding="utf8") as f:
+    f.writelines( data )
+
+#log
+print("Generating log")
+with open(".log.txt", 'a', encoding="utf8") as f:
     now = datetime.datetime.now()
     dateFormat = now.strftime("%Y/%m/%d %H:%M:%S")
-    f.write("----------------------------------------------------------\n")
+    f.write("---------------------------------------------------------------------------------------------------------\n")
     f.write("Created: " + dateFormat +"\n")
     f.write("Blog date: " + date +"\n")
     f.write("Blog folder name: " + folderName +"\n")
@@ -62,4 +72,6 @@ with open(".log.txt", 'a') as f:
     f.write(str(template) +"\n")
     f.write("Dumping postTemplate list:" +"\n")
     f.write(str(postTemplate) +"\n")
-print("Done!")
+    f.write("Dumping data list:" +"\n")
+    f.write(str(data) +"\n")
+print("Log done!")
